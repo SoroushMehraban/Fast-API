ce6ed2b0177a199b732d3942100be4c24ff0ad08
Soroush Mehraban <Smehraban2013@gmail.com>
2022-02-19 21:26:49 +0330

    Get user is added

--- blog/main.py
+++ blog/main.py
@@ -66,10 +66,19 @@ def get_blog(blog_id, response: Response, db: Session = Depends(get_db)):
     return blog
 
 
-@app.post("/user")
+@app.post("/user", response_model=schemas.ShowUser)
 def create_user(request: schemas.User, db: Session = Depends(get_db)):
     new_user = models.User(name=request.name, email=request.email, password=Hash.bcrypt(request.password))
     db.add(new_user)
     db.commit()
     db.refresh(new_user)
     return new_user
+
+
+@app.post('/user/{user_id}', response_model=schemas.ShowUser)
+def get_user(user_id: int,  db: Session = Depends(get_db)):
+    user = db.query(models.User).get(user_id)
+    if user is None:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"User with id {user_id} is not available")
+
+    return user
--- blog/schemas.py
+++ blog/schemas.py
@@ -18,3 +18,11 @@ class User(BaseModel):
     name: str
     email: str
     password: str
+
+
+class ShowUser(BaseModel):
+    name: str
+    email: str
+
+    class Config:
+        orm_mode = True
